1dbcc579f3abbc56d922c661e5a8b678130c22c8
preyank <preyank@ccs.neu.edu>
2019-04-12 20:25:35 -0400

    connection string changed

--- data/db.js
+++ data/db.js
@@ -1,8 +1,8 @@
 module.exports = function () {
     const mongoose = require('mongoose');
-    const databaseName = 'whiteboard-sp19';
+    //const databaseName = 'whiteboard-sp19';
     var   connectionString =
-        'mongodb://localhost/';
-    connectionString += databaseName;
+        'mongodb://heroku_8t1jzp2z:au65f0ppjhkh9ov63ar6jnefj4@ds139576.mlab.com:39576/heroku_8t1jzp2z';
+    //connectionString += databaseName;
     mongoose.connect(connectionString);
 };
